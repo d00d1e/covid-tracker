375806abe5d45ec4aa2f856dcc62e1c783aad743
d00d1e <lee.julianay@gmail.com>
2021-04-19 16:29:23 -0700

    add and style chart component

--- src/api/index.js
+++ src/api/index.js
@@ -8,9 +8,24 @@ export const fetchData = async () => {
       data: { confirmed, recovered, deaths, lastUpdate },
     } = await axios.get(BASE_URL);
 
-    console.log({ confirmed, recovered, deaths, lastUpdate });
+    // console.log({ confirmed, recovered, deaths, lastUpdate });
 
     return { confirmed, recovered, deaths, lastUpdate };
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/daily`);
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modifiedData;
   } catch (err) {
     console.error(err);
   }
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchDailyData } from "../../api";
+import { Line } from "react-chartjs-2";
 import "./Charts.css";
 
 export default function Chart() {
-  return (
-    <div>
-      <h1>Chart</h1>
-    </div>
-  );
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    // console.log(dailyData);
+    fetchAPI();
+  });
+
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) =>
+          new Date(date).toLocaleDateString()
+        ),
+        datasets: [
+          {
+            data: dailyData.map((data) => data.confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map((data) => data.deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255, 0, 0, 0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  return <div className="container">{lineChart}</div>;
 }
